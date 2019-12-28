8ef2ce6744385c24d7bc255913377bb9424ca144
brettm12345 <brettmandler@gmail.com>
2019-12-28 14:18:51 -0500

    Updated select

--- src/components/selectLanguage.tsx
+++ src/components/selectLanguage.tsx
@@ -2,10 +2,35 @@ import Select from 'components/select'
 import { options } from 'data/languages'
 import { FC } from 'react'
 import { Props } from 'react-select'
+import cn from 'ts-classnames'
 import { LanguageOption } from 'types'
 
+import { OptionProps } from '@atlaskit/select'
+
+import Language from './language'
+
+const CustomOption: FC<OptionProps<LanguageOption>> = ({
+  isDisabled,
+  innerProps,
+  label
+}) =>
+  !isDisabled ? (
+    <div
+      className={cn("p-2", "px-3", "hover:bg-gray-600", "select__option")}
+      {...innerProps}
+    >
+      <Language>{label}</Language>
+    </div>
+  ) : null;
+
 const SelectLanguage: FC<Props<LanguageOption>> = props => (
-  <Select {...props} options={options} />
+  <Select
+    {...props}
+    options={options}
+    components={{
+      Option: CustomOption
+    }}
+  />
 );
 
 export default SelectLanguage;
