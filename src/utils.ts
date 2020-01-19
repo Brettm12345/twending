31ae47057a3d5d36a90ee34e2266871a40ed2d23
brettm12345 <brettmandler@gmail.com>
2020-01-19 03:12:34 -0500

    Cleanup and remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,12 @@
 import * as t from 'io-ts'
 import * as RD from '@devexperts/remote-data-ts'
-import type { RemoteData } from '@devexperts/remote-data-ts'
-import fetch from 'unfetch'
+import * as R from 'fp-ts/lib/Record'
 import {
   flow,
   not,
   Endomorphism,
   FunctionN as FN,
 } from 'fp-ts/lib/function'
-import * as R from 'fp-ts/lib/Record'
 import { lookup } from 'fp-ts/lib/Record'
 import {
   filter,
@@ -16,31 +14,20 @@ import {
   array,
   isEmpty,
 } from 'fp-ts/lib/Array'
-import * as T from 'fp-ts/lib/Task'
-import type { Task } from 'fp-ts/lib/Task'
 import { FC, createElement } from 'react'
 import { pipe } from 'fp-ts/lib/pipeable'
-import { Option } from 'fp-ts/lib/Option'
-import { PropsOf } from '@emotion/styled-base/types/helper'
 import { getLastSemigroup } from 'fp-ts/lib/Semigroup'
 import cn from 'ts-classnames'
-
-type Tag = keyof import('@emotion/styled').StyledTags<
-  unknown
->
+import type { Mixed } from 'io-ts'
+import type { RemoteData } from '@devexperts/remote-data-ts'
+import type { Option } from 'fp-ts/lib/Option'
+import type { PropsOf } from '@emotion/styled-base/types/helper'
 
 type JoinRD = <E, A>(
   a: RemoteData<E, A[]>
 ) => Endomorphism<RemoteData<E, A[]>>
 export const joinRD: JoinRD = a => b =>
   pipe(RD.combine(a, b), RD.map(flatten))
-
-type Get = FN<Parameters<typeof fetch>, Task<any>>
-export const get: Get = (...args) =>
-  pipe(
-    () => fetch(...args),
-    T.chain(r => () => r.json())
-  )
 
 interface SelectOption<L extends string> {
   label: L
@@ -72,7 +59,7 @@ type InAny = <A>(xs: A[][]) => FN<[A], boolean>
 export const inAny: InAny = xs => x =>
   pipe(flatten(xs), has(x))
 
-type OneOf = FN<[Array<string>], t.Mixed>
+type OneOf = FN<[Array<string>], Mixed>
 export const oneOf: OneOf = xs =>
   pipe(
     R.fromFoldableMap(getLastSemigroup<string>(), array)(
@@ -87,13 +74,12 @@ type Find = <A>(
 ) => FN<[string], Option<A>>
 export const find: Find = r => k => lookup(k, r)
 
-type Nullable = FN<[t.Mixed], t.UnionC<[t.Mixed, t.NullC]>>
-export const nullable: Nullable = type =>
-  t.union([type, t.null])
-
+type Tag = keyof import('@emotion/styled').StyledTags<
+  unknown
+>
 type TW = <T extends Tag>(
   t: T
-) => (...c: ClassNames[]) => FC<PropsOf<T>>
+) => FN<ClassNames[], FC<PropsOf<T>>>
 export const tw: TW = t => (...c) => ({
   className = '',
   ...p
