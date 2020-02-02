b1ebdaacc6e0b89f72ab0e09f765bc6e3671740f
brettm12345 <brettmandler@gmail.com>
2020-02-02 01:53:58 -0500

    Fix language props

--- src/components/select/select.language.ts
+++ src/components/select/select.language.ts
@@ -7,11 +7,12 @@ const SelectLanguage = ([initialValue, onOptionChange]: [
   OptionType,
   (a: OptionType) => void
 ]) =>
+  typeof window !== 'undefined' &&
   Select({
     blurInputOnSelect: true,
     initialValue,
     onOptionChange,
-    options: options.flatMap(a => a.options),
+    options,
     renderOptionLabel: ({ label }: OptionType) =>
       Language(label)(),
   })
--- src/data/languages/utils.ts
+++ src/data/languages/utils.ts
@@ -2,7 +2,6 @@ import { map } from 'fp-ts/lib/Array'
 import { constant, flow } from 'fp-ts/lib/function'
 import { getOrElse } from 'fp-ts/lib/Option'
 import { pipe } from 'fp-ts/lib/pipeable'
-import { GroupedOptionsType, GroupType } from 'react-select'
 
 import { all } from './constants'
 import {
@@ -25,22 +24,9 @@ export const getColor: GetColor = flow(
   getOrElse(constant(gray[400]))
 )
 
-type MakeGroup = (
-  a: [string, string[]]
-) => GroupType<OptionType>
-const makeGroup: MakeGroup = ([label, values]) =>
-  pipe(values, map(makeOption), options => ({
-    label,
-    options,
-  }))
-
 export const allLanguages = makeOption(all)
 
-export const options: GroupedOptionsType<OptionType> = pipe(
-  [
-    [all, [all]],
-    ['Popular', popular],
-    ['Everything Else', everythingElse],
-  ],
-  map(makeGroup)
+export const options: OptionType[] = pipe(
+  [all, ...popular, ...everythingElse],
+  map(makeOption)
 )
