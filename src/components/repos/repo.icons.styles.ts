905c154ab47c3949f7e781ca19546893723ce001
brettm12345 <brettmandler@gmail.com>
2020-03-03 11:05:41 -0500

    Icon spacing

--- src/components/repos/repo.icons.styles.ts
+++ src/components/repos/repo.icons.styles.ts
@@ -4,9 +4,10 @@ import { tw } from 'src/utils'
 export const item = span(tw('inline-flex', 'mr-4'))
 
 export const icon = svg(
-  tw('fill-current', 'mr-2', '-mt-px')
+  tw('fill-current', 'mx-1', '-mt-px')
 )({
   height: '1.4em',
   viewBox: '0 0 14 16',
+
   width: '1.4em',
 })
